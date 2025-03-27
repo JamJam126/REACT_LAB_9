1d1ba2436c8912e5de4b0ef6d7786bd099388a0c
VRithea <vongrithea126@gmail.com>
2025-03-27 16:12:37 +0700

    finalize

--- exercise 3/src/components/OrderCard.jsx
+++ exercise 3/src/components/OrderCard.jsx
@@ -1,39 +1,46 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
-export default function OrderCard(props, {onQuantityChange}) {
-  const [quantity, setQuantity] = useState(props.order.quantity)
+export default function OrderCard(props) {
+  const { order, index, onQuantityChange } = props;
+
+  const [quantity, setQuantity] = useState(order.quantity);
 
   const handleDecrease = () => {
-
-    if (quantity !== 0) setQuantity(quantity - 1)
-      
-    returnQuantity()
-  }
+    if (quantity > 0) {
+      setQuantity(prevQuantity => {
+        const newQuantity = prevQuantity - 1;
+        onQuantityChange(index, newQuantity); // Update parent with the new quantity
+        return newQuantity;
+      });
+    }
+  };
 
   const handleIncrease = () => {
-
-    setQuantity(quantity + 1)
-    returnQuantity()
-  }
-
-  const returnQuantity = () => {
-
-
-    onQuantityChange(props.index, quantity)
-  }
+    setQuantity(prevQuantity => {
+      const newQuantity = prevQuantity + 1;
+      onQuantityChange(index, newQuantity); // Update parent with the new quantity
+      return newQuantity;
+    });
+  };
 
   return (
     <div className="order">
       <div>
-        <h4>{props.order.product}</h4>
-        <small>{props.order.price}</small>
+        <h4>{order.product}</h4>
+        <small>{order.price}</small>
       </div>
 
       <div className="order-quantity">
-        <div className={`${quantity === 0 ? 'order-button disable' : 'order-button'}`} onClick={handleDecrease}>-</div>
+        <div
+          className={`${quantity === 0 ? "order-button disable" : "order-button"}`}
+          onClick={handleDecrease}
+        >
+          -
+        </div>
         <h4>{quantity}</h4>
-        <div className="order-button" onClick={handleIncrease}>+</div>
+        <div className="order-button" onClick={handleIncrease}>
+          +
+        </div>
       </div>
     </div>
   );
